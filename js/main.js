82fe141137e90f0e050eda5eeab9a81465b35ed5
Jahangir <jahangirxl@gmail.com>
2024-04-15 07:15:53 +0530

    js changes

--- js/main.js
+++ js/main.js
@@ -793,21 +793,21 @@
     window.addEventListener("mousemove", m),
     window.addEventListener("touchstart", m),
     window.addEventListener("touchmove", m),
-    window.addEventListener(
-      "contextmenu",
-      function (e) {
-        e.preventDefault();
-      },
-      !1
-    ),
+    // window.addEventListener(
+    //   "contextmenu",
+    //   function (e) {
+    //     e.preventDefault();
+    //   },
+    //   !1
+    // ),
     (document.onkeydown = function (e) {
-      return (
-        123 != event.keyCode &&
-        (!e.ctrlKey || !e.shiftKey || e.keyCode != "I".charCodeAt(0)) &&
-        (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) &&
-        (!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) &&
-        (!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) &&
-        void 0
-      );
+      // return (
+      //   123 != event.keyCode &&
+      //   (!e.ctrlKey || !e.shiftKey || e.keyCode != "I".charCodeAt(0)) &&
+      //   (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) &&
+      //   (!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) &&
+      //   (!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) &&
+      //   void 0
+      // );
     });
 })(jQuery);
